8cda66fa92045fd4ce1faf8439bfd1a1ed4ccb5e
Austin Cudworth <austincudworth@gmail.com>
2021-01-01 20:58:37 -0800

    Refactor & add second browser window

--- inventory-watcher.py
+++ inventory-watcher.py
@@ -2,52 +2,54 @@
 import json, time, random
 from twilio.rest import Client
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.expected_conditions import presence_of_element_located
 
-url = "https://www.costco.com/xbox-series-x-1tb-console-with-additional-controller.product.100691493.html"
+costcoURL = "https://www.costco.com/xbox-series-x-1tb-console-with-additional-controller.product.100691493.html"
+amazonURL = "https://www.amazon.com/dp/B08H75RTZ8/?coliid=I3T2N56UL91B2I&colid=6ONAQH8MLC04&psc=0&ref_=lv_ov_lig_dp_it"
 
-driver = webdriver.Firefox()
-driver.get(url)
+dr1 = webdriver.Firefox()
+dr2 = webdriver.Firefox()
 
-#wait = WebDriverWait(driver, 10);
-
-login = input('Press return when user is logged in to the desired webpage\n');
+dr1.get(costcoURL)
+dr2.get(amazonURL)
 
 def pause(low, high):
     interval = low + (high - low) * random.random()
     time.sleep(interval)
 
-def sendTextNotification():
+def sendTextNotification(message):
     with open('twilioKeys.json', 'r') as jsonFile:
         data = json.load(jsonFile)
         myClient = Client(data['accountSID'], data['authToken'])
-        message = myClient.messages.create(body='SUCCESS', from_=data['twilioPhone'], to=data['myPhone'])
+        message = myClient.messages.create(body=message, from_=data['twilioPhone'], to=data['myPhone'])
+
+def refresh(driver, iter):
+    if iter:
+        driver.refresh()
+
+def addToCart(driver, iter, elementID, notifierMessage):
+    if iter:
+        try:
+            elem = None
+            elem = driver.find_element_by_id(elementID)
+            if elem:
+                elem.click()
+                sendTextNotification(notifierMessage)
+                iter = False
+        except Exception as err:
+            print('Error occured: ', err)
+    return iter
 
 
-iterate = True  
-while iterate == True:
+iter1 = True
+iter2 = True
 
-    driver.refresh()
-    pause(60, 90)
+login = input('Press return when user is logged in to the desired webpage\n');
 
-    try:
-        #links = driver.find_elements_by_tag_name("a") #Reddit
+while True:
+    refresh(dr1, iter1)
+    refresh(dr2, iter2)
 
-        elem = driver.find_element_by_id("add-to-cart-btn") #Costco
-        if elem:
-            elem.click()
-            sendTextNotification()
-            iterate = False
+    pause(40, 60)
 
-        '''
-        for link in links:
-            if link.get_attribute('href') == target:
-                link.click()
-                iterate = False
-                break
-        '''
-
-    except Exception as err:
-        print('Error occured: ', err)
+    iter1 = addToCart(dr1, iter1, "add-to-cart-btn", "Inventory @ Costco")
+    iter2 = addToCart(dr2, iter2, "add-to-cart-button", "Inventory @ Amazon")
