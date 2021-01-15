c4361ae5b8ae411bcc4ea9cc7d2b83448b48ee88
Austin Cudworth <austincudworth@gmail.com>
2021-01-14 22:06:21 -0800

    Revised inventory watcher script

--- inventory-watcher.py
+++ inventory-watcher.py
@@ -3,53 +3,47 @@ import json, time, random
 from twilio.rest import Client
 from selenium import webdriver
 
-costcoURL = "https://www.costco.com/xbox-series-x-1tb-console-with-additional-controller.product.100691493.html"
-amazonURL = "https://www.amazon.com/dp/B08H75RTZ8/?coliid=I3T2N56UL91B2I&colid=6ONAQH8MLC04&psc=0&ref_=lv_ov_lig_dp_it"
+fo = open('private.json', 'r')
+data = json.load(fo)
+fo.close()
 
-dr1 = webdriver.Firefox()
-dr2 = webdriver.Firefox()
+loginPage = "https://www.costco.com/LogonForm"
+productPage = "https://www.costco.com/xbox-series-x-1tb-console-with-additional-controller.product.100691493.html"
 
-dr1.get(costcoURL)
-dr2.get(amazonURL)
+myDriver = webdriver.Firefox()
 
-def pause(low, high):
+def randPause(low, high):
     interval = low + (high - low) * random.random()
     time.sleep(interval)
 
 def sendTextNotification(message):
-    with open('twilioKeys.json', 'r') as jsonFile:
-        data = json.load(jsonFile)
-        myClient = Client(data['accountSID'], data['authToken'])
-        message = myClient.messages.create(body=message, from_=data['twilioPhone'], to=data['myPhone'])
-
-def refresh(driver, iter):
-    if iter:
-        driver.refresh()
+    myClient = Client(data['twilioAccountSID'], data['twilioAuthToken'])
+    message = myClient.messages.create(body=message, from_=data['twilioPhone'], to=data['myPhone'])
 
 def addToCart(driver, iter, elementID, notifierMessage):
-    if iter:
-        try:
-            elem = None
-            elem = driver.find_element_by_id(elementID)
-            if elem:
-                elem.click()
-                sendTextNotification(notifierMessage)
-                iter = False
-        except Exception as err:
-            print('Error occured: ', err)
+    time.sleep(3)
+    try:
+        driver.find_element_by_id(elementID).click()
+        sendTextNotification(notifierMessage)
+        iter = False
+    except Exception as err:
+        print('Error occured: ', err)
     return iter
 
+def costcoLogin(driver):
+    driver.find_element_by_id('logonId').send_keys(data['costcoEmail'])
+    driver.find_element_by_id('logonPassword').send_keys(data['costcoPassword'])
+    driver.find_element_by_id('logonPassword').submit()
 
-iter1 = True
-iter2 = True
+myDriver.get(loginPage)
 
-login = input('Press return when user is logged in to the desired webpage\n');
+costcoLogin(myDriver)
 
-while True:
-    refresh(dr1, iter1)
-    refresh(dr2, iter2)
+myDriver.get(productPage)
 
-    pause(40, 60)
+iter = True
 
-    iter1 = addToCart(dr1, iter1, "add-to-cart-btn", "Inventory @ Costco")
-    iter2 = addToCart(dr2, iter2, "add-to-cart-button", "Inventory @ Amazon")
+while iter:
+    randPause(15, 20)
+    myDriver.refresh()
+    iter = addToCart(myDriver, iter, "add-to-cart-btn", "Inventory @ Costco")
