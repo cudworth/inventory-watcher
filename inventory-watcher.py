41ef249794929923b169b193b632c7c08cdd90dd
Austin Cudworth <austincudworth@gmail.com>
2021-02-03 10:14:57 -0800

    Add sample credentials.json file

--- inventory-watcher.py
+++ inventory-watcher.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
-import json, time, random
+import json, time
 from twilio.rest import Client
 from selenium import webdriver
-
-fo = open('private.json', 'r')
-data = json.load(fo)
-fo.close()
-
-loginPage = "https://www.costco.com/LogonForm"
-productPage = "https://www.costco.com/xbox-series-x-1tb-console-with-additional-controller.product.100691493.html"
-
-myDriver = webdriver.Firefox()
-
-def randPause(low, high):
-    interval = low + (high - low) * random.random()
-    time.sleep(interval)
 
 def sendTextNotification(message):
     myClient = Client(data['twilioAccountSID'], data['twilioAuthToken'])
@@ -23,10 +10,12 @@ def sendTextNotification(message):
 def addToCart(driver, iter, elementID, notifierMessage):
     time.sleep(3)
     try:
-        driver.find_element_by_id(elementID).click()
-        sendTextNotification(notifierMessage)
-        print('add to cart btn clicked')
-        iter = False
+        elem = driver.find_element_by_id(elementID)
+        if elem and 'out-of-stock' not in elem.get_attribute("class"):
+            elem.click()
+            sendTextNotification(notifierMessage)
+            print('add to cart btn clicked')
+            iter = False
     except Exception as err:
         print('Error occured: ', err)
     return iter
@@ -38,6 +27,16 @@ def costcoLogin(driver):
     driver.find_element_by_id('LogonForm').submit()
     time.sleep(20)
 
+
+fo = open('credentials.json', 'r')
+data = json.load(fo)
+fo.close()
+
+loginPage = "https://www.costco.com/LogonForm"
+productPage = "https://www.costco.com/xbox-series-x-1tb-console-with-additional-controller.product.100691493.html"
+
+myDriver = webdriver.Firefox()
+
 myDriver.get(loginPage)
 
 costcoLogin(myDriver)
@@ -45,8 +44,7 @@ costcoLogin(myDriver)
 myDriver.get(productPage)
 
 iter = True
-
 while iter:
-    randPause(20, 25)
+    time.sleep(25);
     myDriver.refresh()
     iter = addToCart(myDriver, iter, "add-to-cart-btn", "Inventory @ Costco")
